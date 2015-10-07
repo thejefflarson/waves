b3209138ebbe7da76848e18067e773491b3495e7
Jeff Larson <thejefflarson@gmail.com>
2015-10-07 17:57:13 -0400

    ok back to fft

--- javascripts/renderer.js
+++ javascripts/renderer.js
@@ -1,12 +1,11 @@
-// 'use strict';
-
-const detail = 128 * 2;
+const detail = 256;
 
 function Renderer() {
   this.gl = GL.create({antialias: true});
   this.time = 0;
-  this.mesh = GL.Mesh.plane({ coords: true, detailX: detail - 1, detailY: detail - 1 });
+  this.mesh = GL.Mesh.plane({ coords: true });
   this.mesh.computeWireframe();
+  this.displacement = new GL.Texture(detail, detail, {type: this.gl.FLOAT, minFilter: this.gl.NEAREST, magFilter: this.gl.NEAREST });
   load(
     'javascripts/shaders/displacement.fragment.glsl',
     'javascripts/shaders/displacement.vertex.glsl',
@@ -34,18 +33,20 @@ Renderer.prototype = {
   draw : function() {
     var gl = this.gl;
     gl.enable(gl.DEPTH_TEST);
-    gl.enable(gl.BLEND);
-    gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.matrixMode(gl.PROJECTION);
-    gl.loadIdentity();
-    gl.perspective(45, gl.canvas.width / gl.canvas.height, 0.1, 1000);
-    gl.translate(0, 0, -5);
-    gl.rotate(-45, 1, 0, 0);
-    gl.rotate(60, 0, 0, 1);
+    // gl.matrixMode(gl.PROJECTION);
+    // gl.loadIdentity();
+    // gl.perspective(45, gl.canvas.width / gl.canvas.height, 0.1, 1000);
+    // gl.translate(0, 0, -5);
+    // gl.rotate(-45, 1, 0, 0);
+    // gl.rotate(60, 0, 0, 1);
 
     this.display.uniforms({
-      time: this.time
+      time: this.time,
+      size: 250,
+      res: detail,
+      depth: 10,
+      wind: [100.0, 100.0]
     }).draw(this.mesh);
   }
 };
--- javascripts/shaders/displacement.fragment.glsl
+++ javascripts/shaders/displacement.fragment.glsl
@@ -1,6 +1,63 @@
-varying float height;
+uniform vec2 wind;
+uniform float depth;
+uniform float res;
+uniform float size;
+
+const float g  = 9.81;    // gravity m / s ^2
+const float h  = 50.;     // depth of the water
+const float p  = 1000.;   // density of water
+const float F  = 50.;     // fetch
+const float y  = 3.3;     // gamma for JONSWAP
+const float e  = 2.71828; // e
+const float pi = 3.14159; // pi, du
+
+float rand(vec2 co){
+  return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
+
+float rand(float num) {
+  return rand(vec2(num));
+}
+
+float tanh(float x) {
+  return (1.0 - exp(-2.0 * x)) / (1.0 + exp(-2.0 * x));
+}
+
+// corrects for depth of the ocean
+float kitai(float w, float depth) {
+  float w_h = w * sqrt(depth / g);
+  return smoothstep(0., 2.2, w_h);
+}
+
+// frequency of ocean waves
+float jonswap(float w, float U) {
+  float w_p = 22. * pow(g, 2.) / (U * F); // peak frequency
+  float sigma = w <= w_p ? 0.07 : 0.09;
+  float alpha = 0.076 * pow(pow(U, 2.) / (F * g), 0.22);
+  float r = exp(-1. * pow((w - w_p), 2.) /
+            (2. * pow(sigma, 2.) * pow(w_p, 2.)));
+  float s = alpha * pow(g, 2.) / pow(w, 5.) * exp(-5. / 4. * pow(w_p / w, 4.)) * pow(y, r);
+  return s;
+}
+
+float omega(float k, float depth) {
+  return sqrt((g * k + 0.074 / p * pow(k, 3.)) * tanh(k * depth));
+}
+
+// from http://www.dtic.mil/dtic/tr/fulltext/u2/a157975.pdf via Horvath
+float tma(vec2 K, vec2 wind, float depth) {
+  float k = length(K); // wave vector
+  float U = length(wind); // wind speed
+  float w = omega(k, depth);
+  float theta = atan(wind.y / wind.x);
+  // we use tessendorf's spreading function because it is easier
+  float d = theta > -pi / 2. && theta < pi / 2. ? 2. / pi * pow(cos(theta), 2.) : 0.;
+  return jonswap(w, U) * kitai(w, depth) * d;
+}
 
 void main(){
-  // gl_FragColor = vec4(vec3(1.), 1.0);
-  gl_FragColor = vec4(0, 0, height * 10., 1.0);
+  vec2 c = gl_FragCoord.xy - 0.5;
+  vec2 K = 2.0 * pi * vec2(c.x < res * 0.5 ? c.x : c.x - res, c.y < res * 0.5 ? c.y : c.y - res) / size;
+
+  gl_FragColor = vec4(tma(K, wind, depth), 0, 0, 1.0);
 }
